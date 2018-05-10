a3fc5c066ab6e9f22220779bd93ba960e602e3e7
raduconst <radu.constantin@pixelgrade.com>
2018-05-10 13:24:19 +0400

    Removed the footer menu and back to top button closes #4

--- footer.php
+++ footer.php
@@ -17,27 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<?php pixelgrade_footer_the_copyright(); ?><!-- .site-info
-		--><div class="back-to-top-wrapper">
-			<a href="#top" class="back-to-top-button"><?php get_template_part( 'assets/svg/back-to-top' ); ?></a>
-		</div><!--
-		--><?php
-		wp_nav_menu( array(
-			'theme_location' => 'footer',
-			'container'      => '',
-			'menu_class'     => 'nav  nav--footer',
-			'items_wrap'         => '<nav class="footer-menu"><h5 class="screen-reader-text">'.__( 'Footer navigation', 'patch-lite' ).'</h5><ul id="%1$s" class="%2$s">%3$s</ul></nav>',
-			'depth'          => 1,
-			'fallback_cb'    => '',
-		) ); ?>
+		<?php pixelgrade_footer_the_copyright(); ?>
 	</footer><!-- #colophon -->
-	<div class="overlay--search">
-		<div class="overlay__wrapper">
-			<?php get_search_form(); ?>
-			<p><?php _e( 'Begin typing your search above and press return to search. Press Esc to cancel.', 'patch-lite' ); ?></p>
-		</div>
-		<b class="overlay__close"></b>
-	</div>
 </div><!-- #page -->
 
 <div class="mobile-header">
--- functions.php
+++ functions.php
@@ -52,7 +52,6 @@ if ( ! function_exists( 'patch_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'primary' 	=> __( 'Primary Menu', 'patch-lite' ),
-			'footer'    => __( 'Footer Menu', 'patch-lite' ),
 		) );
 
 		/*
