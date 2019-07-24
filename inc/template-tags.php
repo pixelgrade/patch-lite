21d79d29e2981c0abe5006cadac8d960377435d7
madalingorbanescu <madalin@pixelgrade.com>
2019-07-24 13:46:25 +0300

    Add new wp_body_open hook to close #21

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1048,3 +1048,21 @@ if ( ! function_exists( 'patch_lite_parse_content_tags' ) ) {
 		return apply_filters( 'pixelgrade_after_parse_content_tags', $content, $original_content );
 	}
 }
+
+if ( ! function_exists( 'wp_body_open' ) ) :
+	/**
+	 * Fire the wp_body_open action.
+	 *
+	 * Added for backwards compatibility to support pre 5.2.0 WordPress versions.
+	 *
+	 * @since Patch Lite 1.2.2
+	 */
+	function wp_body_open() {
+		/**
+		 * Triggered after the opening <body> tag.
+		 *
+		 * @since Patch Lite 1.2.2
+		 */
+		do_action( 'wp_body_open' );
+	}
+endif;
